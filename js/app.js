68308e2e8a94af30c5f04d89cbc23eb4518b597f
feh-bg-generator <36599080+feh-bg-generator@users.noreply.github.com>
2018-03-01 07:57:42 +0300

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -8,6 +8,9 @@ var canvas = document.getElementById("canvas"),
 var app = new Vue({
     el: '#app',
     data: {
+        ratio: 1600 / 1920,
+        pWidth: 1500,
+        aWidth: 800,
         pPosX: -450,
         pPosY: 0,
         aPosX: 0,
@@ -22,7 +25,7 @@ var app = new Vue({
         render() {
             this.disabled = true;
             this.hero = this.hero.replace(/\s/g, '_');
-            render(`img/heroes/Full_Portrait_${this.hero}.png`, `img/heroes/Full_Attack_${this.hero}.png`, this.pPosX, this.pPosY, this.aPosX, this.aPosY, this.logoPosX, this.logoPosY);
+            render(`img/heroes/Full_Portrait_${this.hero}.png`, `img/heroes/Full_Attack_${this.hero}.png`, this.pPosX, this.pPosY, this.aPosX, this.aPosY, this.logoPosX, this.logoPosY, this.pWidth, Math.round(this.pWidth / this.ratio), this.aWidth,  Math.round(this.aWidth / this.ratio));
         },
         download(link) {
             link.href = canvas.toDataURL('image/jpeg', 1.0);
@@ -31,19 +34,19 @@ var app = new Vue({
     },
     mounted: function() {
         this.disabled = true;
-        render(`img/heroes/Full_Portrait_${this.hero}.png`, `img/heroes/Full_Attack_${this.hero}.png`, this.pPosX, this.pPosY, this.aPosX, this.aPosY, this.logoPosX, this.logoPosY);
+        render(`img/heroes/Full_Portrait_${this.hero}.png`, `img/heroes/Full_Attack_${this.hero}.png`, this.pPosX, this.pPosY, this.aPosX, this.aPosY, this.logoPosX, this.logoPosY, this.pWidth, Math.round(this.pWidth / this.ratio), this.aWidth,  Math.round(this.aWidth / this.ratio));
     }
 })
 
-function render(portrait, attack, pPosX, pPosY, aPosX, aPosY, logoPosX, logoPosY) {
+function render(portrait, attack, pPosX, pPosY, aPosX, aPosY, logoPosX, logoPosY, pWidth, pHeight, aWidth, aHeight) {
     bg.onload = () => {
         ctx.drawImage(bg, 0, 0);
         s1.onload = () => {
-            ctx.drawImage(s1, 0, 0, 1600, 1920, aPosX, aPosY, 800, 920);
+            ctx.drawImage(s1, 0, 0, 1600, 1920, aPosX, aPosY, aWidth, aHeight);
             ctx.fillStyle = 'rgba(0, 0, 0, 0.4)';
             ctx.fillRect(0, 0, 720, 1280);
             s2.onload = () => {
-                ctx.drawImage(s2, 0, 0, 1600, 1920, pPosX, pPosY, 1400, 1680);
+                ctx.drawImage(s2, 0, 0, 1600, 1920, pPosX, pPosY, pWidth, pHeight);
                 logo.onload = () => {
                     ctx.drawImage(logo, logoPosX, logoPosY);
                     app.disabled = false;
